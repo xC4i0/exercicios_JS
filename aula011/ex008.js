fee9f49aada5248ea0c1717bc7ab3a2651be5528
Caio_Carmo <caio.carmo0521@gmail.com>
2023-07-29 14:19:54 -0300

    Exercicio 008 e 009 finalizado

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 80.2
+console.log(`A velocidade do seu carro é ${velocidade} Hm/h`)
+if (velocidade > 60) { // Condição SIMPLES
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+} 
+
+console.log('Dirija sempre usando cinto de segurança!')
