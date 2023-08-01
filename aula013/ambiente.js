f223682eab40452a07f1382b12457b4251fd9581
Caio_Carmo <caio.carmo0521@gmail.com>
2023-08-01 17:40:11 -0300

    Aula 013

---
+++ aula013/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while ( c <= 6)  //Primeiro executa o bloco depois ele faz o teste
+
+
+
+var c = 1
+while ( c <= 20) {
+    console.log(`Passo ${c}`)
+    c++
+}  // Faz o teste primeira depois executa o bloco
+
+
+
+
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
---
+++ aula013/node_4caf481bd8d07.tmp.js
@@ -0,0 +1,25 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c--
+} while ( c <= 6)
+
+
+/*
+var c = 1
+while ( c <= 20) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
