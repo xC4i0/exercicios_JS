f0e0c1acfd1930ddeb11a3f5e366569c2ffe76dc
Caio_Carmo <caio.carmo0521@gmail.com>
2023-08-03 13:59:37 -0300

    Exercicio 01 finalizado modulo E

--- exModuloE/ex01/js/script.js
+++ exModuloE/ex01/js/script.js
@@ -7,14 +7,29 @@ var res = window.document.getElementById('res')
 function calcular() {
     if (num.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         alert('[ERRO] Faltam dados')
+        res.innerHTML = 'Impossivel Contar!'
     } else {
-        res.innerHTML = `Contando: `
+        res.innerHTML = `Contando: <br>`
         let inicio = Number(num.value)
         let final = Number(fim.value)
         let quantos = Number(passo.value)
-
-        for (var c = inicio; c <= final; c += quantos) {
-            res.innerHTML += `${c} `
+        if (quantos <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            quantos = 1
         }
+        if (inicio < final) {
+            // Contagem Crescente
+            for (var c = inicio; c <= final; c += quantos) {
+                res.innerHTML += ` ${c}, \u{1F449}`
+            }
+        } else {
+            // Contagem Regressiva
+            for (let c = inicio; c >= final; c -= quantos) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+
+        }
+        res.innerHTML += `\u{1F3C1}`
+
     }
 }
