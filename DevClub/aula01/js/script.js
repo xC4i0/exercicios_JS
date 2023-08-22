5088867ef5ae2fbf9c58faa6377d125df599fa40
Caio_Carmo <caio.carmo0521@gmail.com>
2023-08-22 14:47:51 -0300

    addeventListener

---
+++ DevClub/aula01/js/script.js
@@ -0,0 +1,9 @@
+
+const input = document.querySelector('#main-input')
+const select = document.querySelector('select')
+const button = document.querySelector('.main-button')
+
+input.addEventListener("keypress", function(teste) {
+    console.log(teste)
+})
+
