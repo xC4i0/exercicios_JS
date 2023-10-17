786bea52c08b0907f8927f71480a01e86e6f8e96
Caio_Carmo <caio.carmo0521@gmail.com>
2023-10-17 12:55:59 -0300

    Parte do front do site de sorteio

---
+++ math/aula01/script.js
@@ -0,0 +1,15 @@
+const result =Math.pow (2, 2)
+console.log(result)
+
+
+const result1 =Math.sqrt(25, 2)
+console.log(result1)
+
+const result2 = Math.PI
+
+const result3 =Math.ceil(3.14) // Arrendonda para cima 
+
+const result4 =Math.floor(3.14) // Arredonda para baixo
+
+const result5 =Math.random(3.14)
+
